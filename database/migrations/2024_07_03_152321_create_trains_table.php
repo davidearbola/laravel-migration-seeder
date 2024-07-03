d55d3a2f7b8826d57de83bc9374f8041a44f7305
davidearbola <davidearbola@gmail.com>
2024-07-03 17:43:00 +0200

    creata migrazione per creare una tabella trains all'interno del db

---
+++ database/migrations/2024_07_03_152321_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('company_name', 100);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('arrival_time');
+            $table->dateTime('departure_time');
+            $table->bigInteger('train_code');
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('in_time');
+            $table->boolean('deleted');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
