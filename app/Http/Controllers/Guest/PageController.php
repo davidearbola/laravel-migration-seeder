090c22e3c9deaee76ccf61d8ae06ffb0000e9eb5
davidearbola <davidearbola@gmail.com>
2024-07-03 17:55:50 +0200

    creo model e controller e modifico la route per usare il controller

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        $trainList = Train::all();
+        $data = [
+            'train' => $trainList
+        ];
+        return view('home', $data);
+    }
+}
